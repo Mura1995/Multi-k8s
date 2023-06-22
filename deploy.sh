a1317920f5d3d7bd3f93650c754d873ebf33e605
Mura <muradinov42@gmail.com>
2023-06-22 15:11:31 +0600

    never give up

--- deploy.sh
+++ deploy.sh
@@ -10,6 +10,6 @@ docker push mura1995/multi-server:$SHA
 docker push mura1995/multi-worker:$SHA
 
 kubectl apply -f k8s
-kubectl set image deployments/server-deployments server=mura1995/multi-server:$SHA
-kubectl set image deployments/client-deployments client=mura1995/multi-client:$SHA
-kubectl set image deployments/worker-deployments worker=mura1995/multi-worker:$SHA
+kubectl set image deployments/server-deployment server=mura1995/multi-server:$SHA
+kubectl set image deployments/client-deployment client=mura1995/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=mura1995/multi-worker:$SHA
