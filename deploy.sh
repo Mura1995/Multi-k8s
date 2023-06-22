caf3512696f7e6a97ea3661f57334098af63261b
Mura <muradinov42@gmail.com>
2023-06-22 11:04:19 +0600

    added deployment scripts

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+docker build -t mura1995/multi-client:latest -t mura1995/multi-client:$SHA -f ./client/Dockerfile ./client
+docekr build -t mura1995/multi-server:latest -t mura1995/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t mura1995/multi-worker:latest -t mura1995/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+docker push mura1995/multi-client:latest
+docker push mura1995/multi-server:latest
+docker push mura1995/multi-worker:latest
+
+docker push mura1995/multi-client:$SHA
+docker push mura1995/multi-server:$SHA
+docker push mura1995/multi-worker:$SHA
+
+kubectl apply -f k8s
+kubectl set image deployments/server-deployments server=mura1995/multi-server:$SHA
+kubectl set image deployments/client-deployments client=mura1995/multi-client:$SHA
+kubectl set image deployments/worker-deployments worker=mura1995/multi-worker:$SHA
